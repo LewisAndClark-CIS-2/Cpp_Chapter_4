d40a22f6bd52318ee6fe82c1a658721068daf6bf
jsteiner207 <jsteiner@stcharlessd.org>
2014-12-15 03:28:19 -0600

    gf

---
+++ ch4AppE02.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number = 0;
+
+    cout << "enter a number so I can square it for you ";
+    cin >> number;
+    int sn = number * number;
+    cout << sn << endl
+            ;
+    return 0;
+}
+
